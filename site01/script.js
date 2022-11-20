5a081c426fee79cb50e5a26e96f853c3280ee3da
lucas vieira conhevalik <lucasconhevalik2310@gmail.com>
2022-11-20 17:36:40 -0300

    site do horário do dia

--- site01/script.js
+++ site01/script.js
@@ -1,1 +1,29 @@
-alert('Olá!')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img =document.getElementById('imagem')
+    var data = new Date()
+    var relogio=data.toLocaleTimeString()
+    
+    
+    
+    
+    msg.innerHTML = `Agora são ${relogio}`
+    if(relogio >=0 && relogio < 12){
+        //Bom dIa
+        img.src = './Design sem nome/1.png'
+        document.body.style.backgroundColor = '#e2cd9f'
+        
+    } 
+    else if ( relogio>= 12 && relogio <18){
+        //Boa Tarde
+        img.src = './Design sem nome/2.png'
+        document.body.style.background ='#F7C943'
+    }
+    else {
+        //boa noite
+        img.src ='./Design sem nome/3.png'
+        document.body.style.background ='#1C1B34'
+        
+    }
+    
+} 
