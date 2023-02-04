5916e72b654a5f3dd639443c634a91f0830daa8a
lucas vieira conhevalik <lucasconhevalik2310@gmail.com>
2023-02-04 18:18:09 -0300

    Update script.js

--- site03/script.js
+++ site03/script.js
@@ -5,11 +5,28 @@ function contar(){
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert ('[Erro] Faltam Dados!')
+        res.innerHTML ='Impossivel contar!'
     } else {
-        res.innerHTML ='contando...'
+        res.innerHTML ='contando...<br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            alert(`Passo invalido! Considerando PASSO 1`)
+            p = 1
+        }
+        if( i< f){
+            //contagem crescente
+            for( let c = i; c<= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c-= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+        
     }
 } 
